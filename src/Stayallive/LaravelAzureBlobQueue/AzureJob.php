d3af7d3cc32d36a7be39460a3d4c62508a98e28f
Squigg <github@squigg.co.uk>
2016-10-08 19:12:05 +0100

    Laravel 5.2 support

--- src/Stayallive/LaravelAzureBlobQueue/AzureJob.php
+++ src/Stayallive/LaravelAzureBlobQueue/AzureJob.php
@@ -53,6 +53,16 @@ class AzureJob extends Job implements JobContract
     }
 
     /**
+     * Fire the job.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $this->resolveAndFire(json_decode($this->getRawBody(), true));
+    }
+
+    /**
      * Delete the job from the queue.
      *
      * @return void
